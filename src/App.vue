6c8d4653b54d11ac3627b46b5bb1d24153ba55bd
misterdenie <tanakorn.sa@psu.ac.th>
2019-08-27 11:11:12 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,62 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+     <TodoList :items="sortedItem" @onItemDone="done"></TodoList>
+     <InputFrom  @onSave="save"></InputFrom>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import TodoList from '@/components/TodoList'
+import InputFrom from '@/components/InputFrom'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    TodoList,
+    InputFrom
+  },
+  data (){
+    return { 
+      items: []
+    }
+    
+  },
+  filters: {
+    caplitalize (val){
+      return val.toUpperCase()
+    }
+  },
+  mounted (){
+    this.items= JSON.parse(localStorage['todoItems'])
+  },
+  computed: {
+    sortedItem(){
+      return this.items.sort((a,b) => {
+        return b.time - a.time
+      }).filter(ele => {
+        return ele.completed === false
+      })
+    }
+  },
+  methods:{
+    done(id){
+      this.items = this.items.map(ele => {
+        if (id === ele.time){
+          ele.completed = true
+        }
+        return ele
+      })
+       localStorage['todoItems'] = JSON.stringify(this.items)
+    },
+    save(text){
+      this.items.push({
+        text: text,
+        time: Date.now(),
+        completed: false
+      })
+      localStorage['todoItems'] = JSON.stringify(this.items)
+    }
   }
 }
 </script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,27 @@
+<template>
+    <ul>
+      <li v-for="item in items" :key="item.time">
+        <button @click="done(item.time)">Done</button> {{item.text | caplitalize }}
+      </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name : 'TodoList',
+    props: {
+        items: {
+            type : Array
+        }
+    },
+    methods: {
+        ItemDone(id) {
+            this.$emit('onItemDone',id)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
